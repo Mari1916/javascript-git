076de2e779705505f64da7ac64e702aba03adc20
Mari1916 <mariana.freitas1916@gmail.com>
2024-07-02 10:04:00 -0300

    Exercicios 02/07

---
+++ 02-07/frase.js
@@ -0,0 +1,16 @@
+/* 
+     Vamos criar um programa, que o usuárioexecute uma função, 
+     e que dentro dessa função exista uma frase.
+*/
+function pularLinha(){
+    document.write("<br>")
+}
+
+function mostra (frase){
+    document.write(frase)
+}
+
+mostra("JavaScript com lógica")
+pularLinha()
+pularLinha()
+mostra("Aprendendo função JavaScript")
