3853a0200ae04310dcf9d6090523f34226d7d81f
Mari1916 <mariana.freitas1916@gmail.com>
2024-07-03 11:21:11 -0300

    exc

--- 03-07/animate-JavaScript/script.js
+++ 03-07/animate-JavaScript/script.js
@@ -1,2 +1,18 @@
-// obté, o botão e  imagem do Dom
+// obtem o botão e a imagem do Dom
 
+const bntMoverImagem = document.getElementById("btnMoverImagem")
+let imagem = document.getElementById("imagem");
+
+//Variável para rastrear o estado atual da posição da imagem
+let isMoved = false
+
+//adiciona um evento de clique ao botão
+btnMoverImagem.addEventListener("click", function() {
+    if (isMoved) {
+        imagem.style.left = "0px";//move para a posição inicial
+    }else {
+        imagem.style.left = "500px"; //move para a dreita
+    }
+    //alterna o estado da posição da imagem
+    isMoved = !isMoved;
+});
